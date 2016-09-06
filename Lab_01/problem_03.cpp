56e86a4b533a0b31c6dc2f01688d451e137b7aea
Derek P Sifford <dereksifford@gmail.com>
2016-09-06 11:03:24 -0400

    Begin lab 1 prob 3

---
+++ Lab_01/problem_03.cpp
@@ -0,0 +1,16 @@
+/**
+ * Program Name: problem_03.cpp
+ * Developer: Derek P Sifford
+ * Date: 06.09.2016
+ * Description: Calculates ticket sales for a football stadium
+ */
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    
+
+    return 0;
+}
