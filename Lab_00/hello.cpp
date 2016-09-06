42c78d5e0a590f02a6be3216f1b59cbb405cd2c1
Derek P Sifford <dereksifford@gmail.com>
2016-09-06 13:18:18 -0400

    Add Lab 0

---
+++ Lab_00/hello.cpp
@@ -0,0 +1,13 @@
+/**
+ * Program Name: Hello.cpp
+ * Developer: Derek P Sifford
+ * Date: 01.09.2016
+ * Description: Validation of my development environment
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "This is from my development environment" << endl;
+    return 0;
+}
